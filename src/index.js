694ea20a2d94b32b8011f753e6e77a220d0217ba
coolmanben <mZmiterizrigi@gmail.com>
2021-03-11 15:40:21 +0300

    init: start task
    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if ( n == undefined ) return 0 ;
+    var n_str = n.toString() ;
+    var res_str = n_str.replace('-' , '') ;
+    return res_str.split('').reverse().join('');
 }
